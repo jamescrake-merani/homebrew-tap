7c92a7953c9f9c9666c1e5d98369dad39d5346e5
James Crake-Merani <james@jamescm.co.uk>
2026-07-25 16:01:20 +0100

    Autocorrected errors.

--- Formula/auto-timesheet/auto-timesheet.rb
+++ Formula/auto-timesheet/auto-timesheet.rb
@@ -1,18 +1,18 @@
 class AutoTimesheet < Formula
-  desc "A CLI application for automatically keeping timesheets."
+  desc "CLI application for automatically keeping timesheets"
   homepage "https://github.com/jamescrake-merani/auto-timesheet"
   url "https://github.com/jamescrake-merani/auto-timesheet/archive/refs/tags/0.1.0.tar.gz"
   sha256 "f77a6a40d02a2b4cb926dc8e356f48930ff4de526fad97f6291f73055d40104c"
   license "GPL-3.0-or-later"
 
-  depends_on "zlib"
-  depends_on "graalvm" => :build
   depends_on "clojure" => :build
+  depends_on "graalvm" => :build
   depends_on "xcodes" => :build
+  depends_on "zlib"
 
   env :std
   def install
-    system "clojure -T:build ci"
+    system "clojure", "-T:build", "ci"
     system "native-image --features=clj_easy.graal_build_time.InitClojureClasses -H:-CheckToolchain -jar target/com.github.jamescrake-merani/auto-timesheet-*.jar -o auto-timesheet"
     bin.install "auto-timesheet"
   end
